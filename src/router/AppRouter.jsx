ce6d290e99e53373ed192aff6ee21c50781add57
Semih TOLGA <109283539+semihtolga@users.noreply.github.com>
2022-11-07 20:44:58 +0300

    first commit

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "../pages/Login";
+import Main from "../pages/Main"
+import Register from "../pages/Register";
+import MovieDetail from "../pages/MovieDetail";
 
 const AppRouter = () => {
-  return <div>AppRouter</div>;
+  return <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Main />}/>
+    <Route path="/login" element={<Login/>}/>
+    <Route path="/register" element={<Register />}/>
+    <Route path="/details/" element={<MovieDetail />}/>
+  </Routes>
+  </BrowserRouter>;
 };
 
 export default AppRouter;
